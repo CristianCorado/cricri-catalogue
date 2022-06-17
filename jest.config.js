2829dd3d7f391ee72d1acc58c6a9f4910284ce58
Cristian Corado <zeeshan.ahmad@sap.com>
2022-06-17 17:57:58 -0600

    Fixed UT configuration with images issues

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   rootDir: './src',
+  moduleNameMapper: {
+    "\\.(css|scss|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(png|jpg|gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
   setupFiles: [
     path.resolve('jest/setupEnzyme.js')
   ]
